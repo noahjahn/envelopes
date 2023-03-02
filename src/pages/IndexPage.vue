1a9d7e4ef728f6e82500bd886c09a55914477091
Noah Jahn <najahn19@gmail.com>
2023-03-01 21:22:23 -0500

    Add showBalance flag on dashboard page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-page class="row items-center"> ${{ balance }} </q-page>
+  <q-page class="row items-center">
+    ${{ showBalance ? balance : '******' }}
+    <q-btn
+      rounded
+      :icon="showBalance ? 'visibility_off' : 'visibility'"
+      @click="showBalance = !showBalance"
+    />
+  </q-page>
 </template>
 
 <script lang="ts">
@@ -11,11 +18,13 @@ export default defineComponent({
   name: 'IndexPage',
   async setup() {
     const balance: Ref<number> = ref(0);
+    const showBalance = ref(false);
 
     balance.value = (await envelopes.balance().get()).current;
 
     return {
       balance,
+      showBalance,
     };
   },
 });
